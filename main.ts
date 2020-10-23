2995ebae50b4ef836f78b4644637fd2dbdc003f3
coconutpotato123 <73363585+coconutpotato123@users.noreply.github.com>
2020-10-23 15:41:40 -0400

    set up images

--- main.ts
+++ main.ts
@@ -1,1 +1,24 @@
+scene.setBackgroundColor(7)
+let playerImg = sprites.duck.duck2
+let projectileImg1 = sprites.food.smallDrumstick
+let projectileImg2 = sprites.food.bigHam
+let Item_Image = sprites.food.smallIceCream
 
+let mySprite = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`)
