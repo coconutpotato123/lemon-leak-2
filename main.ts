2540c4544b9547c10a1b094f3f22761f2f7641b5
coconutpotato123 <73363585+coconutpotato123@users.noreply.github.com>
2020-10-23 15:59:25 -0400

    spawning projectiles

--- main.ts
+++ main.ts
@@ -2,23 +2,22 @@ scene.setBackgroundColor(7)
 let playerImg = sprites.duck.duck2
 let projectileImg1 = sprites.food.smallDrumstick
 let projectileImg2 = sprites.food.bigHam
-let Item_Image = sprites.food.smallIceCream
+let item_Image = sprites.food.smallIceCream
 
-let mySprite = sprites.create(img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`)
+// Create ducky sprite
+let ducky = sprites.create(playerImg, SpriteKind.Player)
+controller.moveSprite(ducky)
+ducky.setFlag(SpriteFlag.StayInScreen, true)
+
+// Spawn projectiles
+game.onUpdateInterval(500, function() {
+    let p = sprites.create(projectileImg1)
+    p.setPosition(randint(0,160), 0)
+    p.setVelocity(0, 50)
+})
+
+game.onUpdateInterval(500, function() {
+    let p = sprites.create(projectileImg2)
+    p.setPosition(0, randint(0,120))
+    p.setVelocity(50, 0)
+})
